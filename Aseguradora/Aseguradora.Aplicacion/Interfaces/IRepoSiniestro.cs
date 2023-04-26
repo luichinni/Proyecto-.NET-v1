7e7e8d264c707491e06b7f9e9c308a60479090f0
Nickylyl <131604162+Nickylyl@users.noreply.github.com>
2023-04-26 11:33:22 -0300

    Update IRepoSiniestro.cs

--- Aseguradora/Aseguradora.Aplicacion/Interfaces/IRepoSiniestro.cs
+++ Aseguradora/Aseguradora.Aplicacion/Interfaces/IRepoSiniestro.cs
@@ -3,6 +3,6 @@ public interface IRepoSiniestro
 {
     public void AgregarSiniestro(Siniestro S);
     public void ModificarSiniestro(Siniestro S);
-    public void EliminarSiniestro(Siniestro S);
+    public void EliminarSiniestro(int Id);
     public List<Siniestro> ListarSiniestros();
 }
