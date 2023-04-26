cb71da42cdfb2ccbb1243efc379de16f7de899e8
Nickylyl <131604162+Nickylyl@users.noreply.github.com>
2023-04-26 11:22:44 -0300

    Update Persona.cs

--- Aseguradora/Aseguradora.Aplicacion/Entidades/Persona.cs
+++ Aseguradora/Aseguradora.Aplicacion/Entidades/Persona.cs
@@ -3,7 +3,7 @@ namespace Aseguradora.Aplicacion;
 public abstract class Persona
 {
     public abstract int ID { get; set; }
-    public abstract string DNI {get;set;}
+    public abstract int DNI {get;set;}
     public abstract string Apellido { get; set; }
     public abstract string Nombre { get; set; }
     public abstract string? Telefono { get; set; }
