fc536be835e1487f5e33f96b3b9b7627aa9dd08d
Nickylyl <131604162+Nickylyl@users.noreply.github.com>
2023-04-26 11:34:50 -0300

    Update IRepoTitular.cs

--- Aseguradora/Aseguradora.Aplicacion/Interfaces/IRepoTitular.cs
+++ Aseguradora/Aseguradora.Aplicacion/Interfaces/IRepoTitular.cs
@@ -2,6 +2,6 @@ namespace Aseguradora.Aplicacion;
 public interface IRepoTitular{
     public void AgregarTitular(Titular T);
     public void ModificarTitular(Titular T);
-    public void EliminarTitular(Titular T);
+    public void EliminarTitular(int Id);
     public List<Titular> ListarTitulares();
 }
