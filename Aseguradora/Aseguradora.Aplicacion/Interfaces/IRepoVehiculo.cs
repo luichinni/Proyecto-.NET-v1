b9644d981ef71495afee393b8e03d7df53b855c5
Nickylyl <131604162+Nickylyl@users.noreply.github.com>
2023-04-26 11:35:53 -0300

    Update IRepoVehiculo.cs

--- Aseguradora/Aseguradora.Aplicacion/Interfaces/IRepoVehiculo.cs
+++ Aseguradora/Aseguradora.Aplicacion/Interfaces/IRepoVehiculo.cs
@@ -3,6 +3,6 @@ public interface IRepoVehiculo
 {
     public void AgregarVehiculo(Vehiculo V);
     public void ModificarVehiculo(Vehiculo V);
-    public void EliminarVehiculo(Vehiculo V);
+    public void EliminarVehiculo(int Id);
     public List<Vehiculo> ListarVehiculos();
 }
