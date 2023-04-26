dfb78773857079be160e18f536b4b787011f680a
Nickylyl <131604162+Nickylyl@users.noreply.github.com>
2023-04-26 11:31:23 -0300

    Update IRepoPoliza.cs

--- Aseguradora/Aseguradora.Aplicacion/Interfaces/IRepoPoliza.cs
+++ Aseguradora/Aseguradora.Aplicacion/Interfaces/IRepoPoliza.cs
@@ -3,6 +3,6 @@ public interface IRepoPoliza
 {
     public void AgregarPoliza(Poliza P);
     public void ModificarPoliza(Poliza P);
-    public void EliminarPoliza(Poliza P);
+    public void EliminarPoliza(int Id);
     public List<Poliza> ListarPolizas();
 }
